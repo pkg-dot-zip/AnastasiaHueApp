5123aeb2a27f7e81908e28cd41b4ce873b7ce655
pkg-dot-zip <simon@pkg-dot-zip.com>
2024-11-25 16:09:52 +0100

    Can now retrieve a response from the hue emulator

--- AnastasiaHueApp/ViewModels/MainViewModel.cs
+++ AnastasiaHueApp/ViewModels/MainViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using Microsoft.Extensions.Logging;
 
 namespace AnastasiaHueApp.ViewModels;
@@ -7,6 +8,27 @@ public partial class MainViewModel(
     ILogger<MainViewModel> logger)
     : BaseViewModel
 {
-    [ObservableProperty]
-    private string _text = "TestText!";
+    [ObservableProperty] private string _text = "TestText!";
+    [ObservableProperty] private string _boxText = string.Empty;
+
+    private static readonly HttpClient HttpClient = new()
+    {
+        BaseAddress = new Uri("http://localhost/api/"), // NOTE: If using port 80 no port needs to be specified.
+    };
+
+    [RelayCommand]
+    private async Task RetrieveBridgeConfig()
+    {
+        try
+        {
+            var response = await HttpClient.GetAsync("newdeveloper");
+            response.EnsureSuccessStatusCode();
+            var respString = await response.Content.ReadAsStringAsync();
+            BoxText = respString;
+        }
+        catch (HttpRequestException e)
+        {
+            logger.LogError(e, "Code {0}. Returning null.", e.StatusCode);
+        }
+    }
 }
