f6fa54da02d75a1588ba64db53feb89b45212c01
pkg-dot-zip <simon@pkg-dot-zip.com>
2024-11-26 17:14:11 +0100

    Added docs to IntExtensions

--- AnastasiaHueApp/Util/Extensions/IntExtensions.cs
+++ AnastasiaHueApp/Util/Extensions/IntExtensions.cs
@@ -5,5 +5,12 @@
 /// </summary>
 public static class IntExtensions
 {
-    public static bool IsInRange(this int it, int a, int b) => it >= a && it <= b;
+    /// <summary>
+    /// Checks whether a number <paramref name="n"/> is in the range of <paramref name="a"/>-<paramref name="b"/>. This also returns true if <paramref name="n"/> equals <paramref name="a"/> or <paramref name="b"/>.
+    /// </summary>
+    /// <param name="n">Number to check for.</param>
+    /// <param name="a">Lower-bounds.</param>
+    /// <param name="b">Upper-bounds.</param>
+    /// <returns><see langword="true"/> if <paramref name="n"/> is in the range of <paramref name="a"/>-<paramref name="b"/>. This also returns true if <paramref name="n"/> equals <paramref name="a"/> or <paramref name="b"/>. In all other cases it returns <see langword="false"/>.</returns>
+    public static bool IsInRange(this int n, int a, int b) => n >= a && n <= b;
 }
