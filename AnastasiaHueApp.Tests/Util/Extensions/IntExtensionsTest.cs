f0471c9d7e3c396f38fb1996bb4255cc163a56b0
pkg-dot-zip <simon@pkg-dot-zip.com>
2024-11-26 17:23:56 +0100

    Added unit tests for IsInRange

---
+++ AnastasiaHueApp.Tests/Util/Extensions/IntExtensionsTest.cs
@@ -0,0 +1,37 @@
+﻿using AnastasiaHueApp.Util.Extensions;
+using FluentAssertions;
+using JetBrains.Annotations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AnastasiaHueApp.Tests.Util.Extensions;
+
+[TestClass]
+[TestSubject(typeof(IntExtensions))]
+public class IntExtensionsTest
+{
+    #region IsInRange
+
+    [TestMethod]
+    [DataRow(0, int.MinValue, int.MaxValue)] // N zero.
+    [DataRow(2, 1, 3)] // All positive
+    [DataRow(1, 1, 1)] // All equal.
+    [DataRow(-1, -2, 0)] // N negative.
+    [DataRow(-2, -3, -1)] // All negative.
+    public void IsInRange_NBetweenAAndB_ReturnsTrue(int n, int a, int b)
+    {
+        n.IsInRange(a, b).Should().BeTrue();
+    }
+
+    [TestMethod]
+    [DataRow(int.MinValue, 0, int.MaxValue)] // Far off.
+    [DataRow(0, 1, 2)] // 1 off, all positive.
+    [DataRow(-1, 0, 1)] // N negative.
+    [DataRow(-10, -9, -8)] // 1 off, all negative.
+    public void IsInRange_NNotBetweenAAndB_ReturnsFalse(int n, int a, int b)
+    {
+        n.IsInRange(a, b).Should().BeFalse();
+    }
+
+    #endregion
+
+}
