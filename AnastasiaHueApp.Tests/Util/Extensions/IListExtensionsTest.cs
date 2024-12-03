b93255ccebc3f65a9aedb48365941c879b74ad99
pkg-dot-zip <simon@pkg-dot-zip.com>
2024-12-03 11:55:13 +0100

    Added tests for IListExtensions

---
+++ AnastasiaHueApp.Tests/Util/Extensions/IListExtensionsTest.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AnastasiaHueApp.Util.Extensions;
+using FluentAssertions;
+using JetBrains.Annotations;
+
+namespace AnastasiaHueApp.Tests.Util.Extensions;
+
+[TestClass]
+[TestSubject(typeof(IListExtensions))]
+// ReSharper disable once InconsistentNaming
+public class IListExtensionsTests
+{
+    #region AddAll
+
+    [TestMethod]
+    [DataRow(new int[] { 1, 2, 3 })]
+    [DataRow(new int[] { 8, 2, 3 })]
+    [DataRow(new int[] { -2398, 21985, 12598 })]
+    [DataRow(new int[] { -298, 215, -198 })]
+    public void AddAllEnumerable_IntData_Contains_IntData_ReturnTrue(int[] value)
+    {
+        // Arrange.
+        var list = new List<int>();
+        var listToAddFrom = value.ToList();
+
+        // Act.
+        // ReSharper disable once PossibleMultipleEnumeration
+        list.AddAll(listToAddFrom);
+
+        // Assert.
+        list.Count.Should().NotBe(0);
+        // ReSharper disable once ForeachCanBePartlyConvertedToQueryUsingAnotherGetEnumerator
+        foreach (var i in listToAddFrom)
+        {
+            list.Contains(i).Should().BeTrue();
+        }
+    }
+
+    [TestMethod]
+    [DataRow(new int[] { 1, 2, 3 })]
+    [DataRow(new int[] { 8, 2, 3 })]
+    [DataRow(new int[] { -2398, 21985, 12598 })]
+    [DataRow(new int[] { -298, 215, -198 })]
+    public void AddAllParams_IntData_Contains_IntData_ReturnTrue(int[] value)
+    {
+        // Arrange.
+        var list = new List<int>();
+
+        // Act.
+        // ReSharper disable once PossibleMultipleEnumeration
+        list.AddAll(value);
+
+        // Assert.
+        list.Count.Should().NotBe(0);
+        // ReSharper disable once ForeachCanBePartlyConvertedToQueryUsingAnotherGetEnumerator
+        foreach (var i in value)
+        {
+            list.Contains(i).Should().BeTrue();
+        }
+    }
+
+    [TestMethod]
+    [DataRow(1)]
+    [DataRow(123)]
+    [DataRow(-1241)]
+    [DataRow(int.MaxValue)]
+    [DataRow(int.MinValue)]
+    public void AddAllEnumerable_Int_EndsWith_Int_ReturnTrue(int value)
+    {
+        // Arrange.
+        var list = new List<int> { 98, 912, 173, 123 };
+        var listToAddFrom = new List<int> { value };
+        
+        // Act.
+        list.AddAll(listToAddFrom);
+
+        // Assert.
+        listToAddFrom.Last().Should().Be(value);
+    }
+
+    [TestMethod]
+    [DataRow(1)]
+    [DataRow(123)]
+    [DataRow(-1241)]
+    [DataRow(int.MaxValue)]
+    [DataRow(int.MinValue)]
+    public void AddAllParams_Int_EndsWith_Int_ReturnTrue(int value)
+    {
+        // Arrange.
+        var list = new List<int> { 98, 912, 173, 123 };
+        var listToAddFrom = new[] { value };
+
+        // Act.
+        list.AddAll(listToAddFrom);
+
+        // Assert.
+        listToAddFrom.Last().Should().Be(value);
+    }
+
+    #endregion
+}
--- AnastasiaHueApp/Util/Extensions/IListExtensions.cs
+++ AnastasiaHueApp/Util/Extensions/IListExtensions.cs
@@ -13,10 +13,7 @@ public static class IListExtensions
     /// <typeparam name="T"><see langword="type"/> of <seealso cref="elements"/></typeparam>
     /// <param name="collection">Collection to add the <paramref name="elements"/> to.</param>
     /// <param name="elements">Elements to add to <paramref name="collection"/>.</param>
-    public static void AddAll<T>(this IList<T> collection, params T[] elements)
-    {
-        collection.AddAll(elements as IEnumerable<T>);
-    }
+    public static void AddAll<T>(this IList<T> collection, params T[] elements) => collection.AddAll(elements as IEnumerable<T>);
 
     /// <summary>
     /// Adds all of the <paramref name="elements"/> to the end of the <paramref name="collection"/>.
