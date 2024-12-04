5dc0eec82d34300f2d417f833b4a09488b3252f1
pkg-dot-zip <simon@pkg-dot-zip.com>
2024-12-04 10:42:46 +0100

    Now maximize window on start (on Windows)

--- AnastasiaHueApp/MauiProgram.cs
+++ AnastasiaHueApp/MauiProgram.cs
@@ -6,6 +6,7 @@ using AnastasiaHueApp.Util.Shell;
 using AnastasiaHueApp.ViewModels;
 using CommunityToolkit.Maui;
 using Microsoft.Extensions.Logging;
+using Microsoft.Maui.LifecycleEvents;
 using PanCardView;
 using Serilog;
 
@@ -59,6 +60,33 @@ namespace AnastasiaHueApp
             builder.Services.AddSingleton<IPreferences>(Preferences.Default);
             builder.Services.AddSingleton<IPreferencesHandler, PreferencesHandler>();
             builder.Services.AddSingleton<IShellContainer, ShellContainer>();
+
+
+            // SEE: https://blog.verslu.is/maui/full-screen-disable-minimize-maximize-for-net-maui-windows-apps/
+#if WINDOWS
+            builder.ConfigureLifecycleEvents(events =>
+            {
+                // Make sure to add "using Microsoft.Maui.LifecycleEvents;" in the top of the file 
+                events.AddWindows(windowsLifecycleBuilder =>
+                {
+                    windowsLifecycleBuilder.OnWindowCreated(window =>
+                    {
+                        window.ExtendsContentIntoTitleBar = false;
+                        var handle = WinRT.Interop.WindowNative.GetWindowHandle(window);
+                        var id = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(handle);
+                        var appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(id);
+                        switch (appWindow.Presenter)
+                        {
+                            case Microsoft.UI.Windowing.OverlappedPresenter overlappedPresenter:
+                                // overlappedPresenter.SetBorderAndTitleBar(false, false);
+                                overlappedPresenter.Maximize();
+                                overlappedPresenter.IsMaximizable = false; // Disables maximize button.
+                                break;
+                        }
+                    });
+                });
+            });
+#endif
 
             return builder.Build();
         }
