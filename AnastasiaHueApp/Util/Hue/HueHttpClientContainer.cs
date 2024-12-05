448d96a6897e0dc962ebe9ac9073ca273b3b0dec
pkg-dot-zip <simon@pkg-dot-zip.com>
2024-12-05 16:32:12 +0100

    Now use emulator again

--- AnastasiaHueApp/Util/Hue/HueHttpClientContainer.cs
+++ AnastasiaHueApp/Util/Hue/HueHttpClientContainer.cs
@@ -5,11 +5,11 @@ public class HueHttpClientContainer : IHttpClientContainer
     private static readonly HttpClient ContainedClient = new()
     {
 #if ANDROID
-        //BaseAddress = new Uri("http://10.0.2.2/api/"), // NOTE: Emulator. If using port 80 no port needs to be specified.
-        BaseAddress = new Uri("http://192.168.1.179/api/"), // NOTE: Hardware. If using port 80 no port needs to be specified
+        BaseAddress = new Uri("http://10.0.2.2/api/"), // NOTE: Emulator. If using port 80 no port needs to be specified.
+        //BaseAddress = new Uri("http://192.168.1.179/api/"), // NOTE: Hardware. If using port 80 no port needs to be specified
 #elif WINDOWS
-        //BaseAddress = new Uri("http://localhost/api/"), // NOTE: Emulator. If using port 80 no port needs to be specified.
-        BaseAddress = new Uri("http://192.168.1.179/api/"), // NOTE: Hardware. If using port 80 no port needs to be specified.
+        BaseAddress = new Uri("http://localhost/api/"), // NOTE: Emulator. If using port 80 no port needs to be specified.
+        //BaseAddress = new Uri("http://192.168.1.179/api/"), // NOTE: Hardware. If using port 80 no port needs to be specified.
 #else
 
 #endif
